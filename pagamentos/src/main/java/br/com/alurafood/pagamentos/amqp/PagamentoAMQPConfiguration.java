9e29013d0e2a7c74e00fdf629267208c0c325007
ezequielmmarques <ezequielmmarques@gmail.com>
2023-04-16 18:12:24 -0300

    User root and root banco mysql

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/amqp/PagamentoAMQPConfiguration.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/amqp/PagamentoAMQPConfiguration.java
@@ -3,6 +3,10 @@ package br.com.alurafood.pagamentos.amqp;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +19,15 @@ public class PagamentoAMQPConfiguration {
     public Queue queuePagamento() {
         // return new Queue(QUEUE_PAGAMENTO_CONCLUIDO, false);
         return QueueBuilder.nonDurable(QUEUE_PAGAMENTO_CONCLUIDO).build();
+    }
+
+    @Bean
+    public RabbitAdmin criaRabbitAdmin(ConnectionFactory conn) {
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializaAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
     }
 }
